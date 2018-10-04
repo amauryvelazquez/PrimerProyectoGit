d5227e0734777019fd322a74276ab9bab7c60b68
Clases <Clases@A-000-4307>
2018-10-04 14:13:00 -0500

    SegundoCommit

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -4,6 +4,12 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+ * 
+ * @author Bryan
+ * 
+ * 
+ */
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
